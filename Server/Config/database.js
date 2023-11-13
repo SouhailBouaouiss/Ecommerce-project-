e093feac969a8b478ece617ac9e59cb2bc58e792
Souhail Bouaouiss <souhailbouaouiss0@gmail.com>
2023-11-13 10:47:58 +0100

    update

--- Server/Config/database.js
+++ Server/Config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { Users } from "../Models/User.js";
 const url =
-  "mongodb+srv://souhail:zev-3aMFq8MclZqFMKVk@firstcluster.aihbxtp.mongodb.net/";
+  "mongodb+srv://souhail:3aMFq8MclZqFMKVk@firstcluster.aihbxtp.mongodb.net/";
 
 const connecting = async () => {
   await mongoose.connect(url, { dbName: "3WA_Ecom" });
