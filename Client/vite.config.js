24e1f1806711786eb55e217bb6731aabbaf1a795
Souhail Bouaouiss <souhailbouaouiss0@gmail.com>
2024-01-30 20:33:30 +0100

    wowo

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Specify the output directory for your build
+    outDir: "dist",
+    // Minify the output (optional)
+    minify: true,
+    // Set this to true if you want to generate sourcemaps for your production build
+    sourcemap: false,
+    // Specify additional Rollup options
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        // Provide a custom name for the global variable when using 'script' tag
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
